57c057799e2f71fa6a80d9fa069c075d50c9395f
lym6692575 <82617535@qq.com>
2022-02-25 22:30:11 +0800

    20220225-3 原方案无法斜向操作,修改为数组保存全局监听键盘,下一步实现判断数组数据改变位置

--- src/views/HomeTest.vue
+++ src/views/HomeTest.vue
@@ -12,43 +12,32 @@ export default {
     return {
       topVar: "50%",
       leftVar: "50%",
+      keyCodeArry: [],
     };
   },
 
   methods: {
-    // 移动方向
-    changePosition(e) {
-      let key = e.keyCode; // 根据不同按键实现不同的功能
-      let topPosition = parseInt(this.topVar);
-      let leftPositon = parseInt(this.leftVar);
-      switch (key) {
-        // up
-        case 40:
-          console.log(topPosition + 1 + "%");
-          this.topVar = topPosition + 1 + "%";
-          break;
-        // down
-        case 38:
-          console.log(topPosition - 1 + "%");
-          this.topVar = topPosition - 1 + "%";
-          break;
-        // left
-        case 37:
-          console.log(leftPositon - 1 + "%");
-          this.leftVar = leftPositon - 1 + "%";
-          break;
-        // right
-        case 39:
-          console.log(leftPositon + 1 + "%");
-          this.leftVar = leftPositon + 1 + "%";
-          break;
-        // up and left
-        case 13 && 17:
-          console.log("1");
-          break;
-        default:
-          break;
+    // 按下增加键盘监听数组keyCodeArry数据
+    addKeyCodeArry(num, arr) {
+      var check = 0;
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == num) {
+          check = 1;
+        }
       }
+      if (check == 0) {
+        arr.push(num);
+      }
+      return arr;
+    },
+    // 松开键盘清空键盘监听数组keyCodeArry数据
+    deletKeyCodeArry(num, arr) {
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == num) {
+          arr.splice(i, 1);
+        }
+      }
+      return arr;
     },
   },
 
@@ -56,11 +45,16 @@ export default {
     // 全局监听键盘按下事件
     var _this = this;
     document.onkeydown = function (e) {
-      console.log(e.keyCode); // 取到按下的具体键
-      _this.changePosition(e);
-      // if (13 == key && e.ctrlKey) {
-      //   console.log("1");
-      // }
+      let oEvent = e || event;
+      let keyCode = oEvent.keyCode;
+      _this.keyCodeArry = _this.addKeyCodeArry(keyCode, _this.keyCodeArry);
+      console.log(_this.keyCodeArry);
+    };
+    document.onkeyup = function (e) {
+      let oEvent = e || event;
+      let keyCode = oEvent.keyCode;
+      _this.keyCodeArry = _this.deletKeyCodeArry(keyCode, _this.keyCodeArry);
+      console.log(_this.keyCodeArry);
     };
   },
 };
